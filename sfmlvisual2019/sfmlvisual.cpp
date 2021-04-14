cac7cdd8f467b23fcb4a8aa89fc61d1db60a5645
Maciej Matyka <maciej.matyka@gmail.com>
2021-04-14 18:11:25 +0200

    Pełny projekt VS2019 (trzeba ustawić katalogi jak na filmie!)

---
+++ sfmlvisual2019/sfmlvisual.cpp
@@ -0,0 +1,24 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(600, 600), "SFML works!");
+    sf::CircleShape shape(300.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
